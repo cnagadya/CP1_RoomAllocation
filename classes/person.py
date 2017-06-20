921899e5a1fec16eaa0cd052d383061b10490741
Christine Ritah Nagadya <wonder_woman@Christines-MacBook-Pro.local>
2017-06-20 20:17:03 +0300

    Modifications to person.py

--- classes/person.py
+++ classes/person.py
@@ -1,3 +1,9 @@
+#! usr/bin/python3
+import sys
+import os
+# print(os.path.dirname(os.path.realpath(__file__)))
+
+
 class Person(object):
     def __init__(self, person_name, person_type, person_id):
         self.person_name = person_name
@@ -5,16 +11,16 @@ class Person(object):
         self.person_id = person_id
 
     def __repr__(self):
-        return repr( self.person_name )
+        return repr((str(self.person_id) + " " + self.person_name))
 
 
-class Fellow( Person ):
+class Fellow(Person):
     def __init__(self, person_name, person_type, person_id):
-        super().__init__( person_name, person_type, person_id )
+        super().__init__(person_name, person_type, person_id)
         self.person_type = "FELLOW"
 
 
-class Staff( Person ):
+class Staff(Person):
     def __init__(self, person_name, person_type, person_id):
-        super().__init__( person_name, person_type, person_id )
+        super().__init__(person_name, person_type, person_id)
         self.person_type = "STAFF"
