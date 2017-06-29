d1e92a194f5d4ba21681e3e38b87a0071c90a67d
Christine Ritah Nagadya <wonder_woman@Christines-MacBook-Pro.local>
2017-06-29 18:22:25 +0300

    Removing redundant code from the db file

--- database/database.py
+++ database/database.py
@@ -1,31 +1,7 @@
-import os
-import sys
-from sqlalchemy import Column, ForeignKey, Integer, String, BLOB
+from sqlalchemy import Column, BLOB
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
-from sqlalchemy import create_engine
 
 Base = declarative_base()
-
-
-# class Persons( Base ):
-#     __tablename__ = 'persons'
-#     id = Column( Integer, primary_key=True )
-#     person_name = Column( String( 250 ), nullable=False )
-#     person_type = Column( String( 250 ), nullable=False )
-#     person_id = Column( Integer, nullable=False )
-#
-#     rooms = relationship( "Rooms", back_populates="peoples" )
-#
-#
-# class Rooms( Base ):
-#     __tablename__ = 'rooms'
-#     id = Column( Integer, primary_key=True )
-#     room_name = Column( String( 250 ), nullable=False )
-#     room_type = Column( String( 250 ), nullable=False )
-#     persons_id = Column( Integer, ForeignKey( 'persons.id' ) )
-#     peoples = relationship( "Persons", back_populates="rooms" )
-
 
 class State(Base):
     __tablename__ = "amitystate"
