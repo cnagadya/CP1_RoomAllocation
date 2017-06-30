94b30d1d7fc8f3c61523985f51f7591dfe5e2490
Christine Ritah Nagadya <wonder_woman@Christines-MacBook-Pro.local>
2017-06-30 16:34:34 +0300

    Fixing the inheritance for Room and Pesron super class

--- classes/person.py
+++ classes/person.py
@@ -2,7 +2,7 @@
 class Person(object):
     def __init__(self, person_name, person_type, person_id):
         self.person_name = person_name
-        self.person_type = person_type
+        self.person_type = None
         self.person_id = person_id
 
     def __repr__(self):
--- classes/room.py
+++ classes/room.py
@@ -1,18 +1,19 @@
 #! usr/bin/python3
 class Room( object ):
-    def __init__(self, room_name, room_type, capacity):
+    def __init__(self, room_name):
         self.room_name = room_name
-        self.room_type = room_type
-        self.capacity = capacity
+
 
 
 class Office( Room ):
     def __init__(self, room_name, room_type, capacity):
-        super().__init__( room_name, room_type, capacity )
+        Room.__init__(self, room_name )
+        self.room_type = "Office"
         self.capacity = 6
 
 
 class LivingSpace( Room ):
     def __init__(self, room_name, room_type, capacity):
-        super().__init__( room_name, room_type, capacity )
+        Room.__init__( room_name )
+        self.room_type = "Living Space"
         self.capacity = 4
